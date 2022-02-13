341138ad366dd46235d5d6df003935e71ec7fa56
zabl <zzzabl@gmail.com>
2022-02-13 16:43:03 +0300

    use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use rand::Rng;
-use std::net::UdpSocket;
-use std::time::Duration;
-use std::{env, thread};
+use std::env;
+use tokio::net::UdpSocket;
+use tokio::time::{sleep, Duration};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
-    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
     loop {
         let value = rand::thread_rng().gen::<f32>();
-        if let Err(e) = socket.send_to(&value.to_be_bytes(), format!("{}:{}", args[1], args[2])) {
+        if let Err(e) = socket
+            .send_to(&value.to_be_bytes(), format!("{}:{}", args[1], args[2]))
+            .await
+        {
             println!("Ошибка отправки {}", e.to_string());
         } else {
             println!("Ok");
         }
-        thread::sleep(Duration::new(3, 0));
+        sleep(Duration::from_secs(3)).await;
     }
 }
